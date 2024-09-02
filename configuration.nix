7113a0ef515b0be3b7f3a5fb530f2738a35c8e96
Marcin Freitag <marcin.freitag@outlook.com>
2024-09-02 18:10:42 +0200

    enabled auto-upgrade service without forced reboot, since I tend to reboot the machine every day

--- configuration.nix
+++ configuration.nix
@@ -130,4 +130,10 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
+  # Enable automatic upgrades
+  system.autoUpgrade = {
+    enable = true;
+    allowReboot = false;
+  };
+
 }
