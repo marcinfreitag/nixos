93035d7cfc9964a10fe1b43597dd640eee93e867
Marcin Freitag <marcin.freitag@outlook.com>
2026-04-23 13:26:32 +0200

    added shell aliases to home-manager

--- dot_config/nixos/home.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs,  ...}:
-
-{
-  home = {
-    username = "{{ .linuxUserName }}";
-    homeDirectory = "/home/{{ .linuxUserName }}";
-  }
-}
-# vim: set tabstop=2 shiftwidth=2 expandtab :
